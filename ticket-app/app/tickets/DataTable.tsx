432f0bb5eb5a9e164216744c9b705dea76a6d12a
TomoyaWaniko <alligatorfree12@gmail.com>
2024-05-11 13:40:58 +0900

    update

--- ticket-app/app/tickets/DataTable.tsx
+++ ticket-app/app/tickets/DataTable.tsx
@@ -1,5 +1,7 @@
 import { Ticket } from '@prisma/client';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import TicketStatusBadge from '@/components/TicketStatusBadge';
+import TicketPriority from '@/components/TicketPriority';
 
 interface Props {
   tickets: Ticket[];
@@ -13,9 +15,13 @@ const DataTable = ({ tickets }: Props) => {
           <TableHeader>
             <TableRow>
               <TableHead>title</TableHead>
-              <TableHead>status</TableHead>
-              <TableHead>priority</TableHead>
-              <TableHead>crated at</TableHead>
+              <TableHead>
+                <div className={'flex justify-center'}>status</div>
+              </TableHead>
+              <TableHead>
+                <div className={'flex justify-center'}>priority</div>
+              </TableHead>
+              <TableHead>created at</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
@@ -23,9 +29,25 @@ const DataTable = ({ tickets }: Props) => {
               ? tickets.map((ticket: Ticket) => (
                   <TableRow key={ticket.id} data-href={'/'}>
                     <TableCell>{ticket.title}</TableCell>
-                    <TableCell>{ticket.status}</TableCell>
-                    <TableCell>{ticket.priority}</TableCell>
-                    <TableCell>{ticket.createdAt.toLocaleDateString()}</TableCell>
+                    <TableCell>
+                      <div className={'flex justify-center'}>
+                        <TicketStatusBadge status={ticket.status} />
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      <div className={'flex justify-center'}>
+                        <TicketPriority priority={ticket.priority} />
+                      </div>
+                    </TableCell>
+                    <TableCell>
+                      {ticket.createdAt.toLocaleDateString('ja-JP', {
+                        year: '2-digit',
+                        month: '2-digit',
+                        day: '2-digit',
+                        hour: 'numeric',
+                        minute: '2-digit',
+                      })}
+                    </TableCell>
                   </TableRow>
                 ))
               : null}
