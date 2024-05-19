476728286619d6aa96504640059c0f0e1d8a0392
TomoyaWaniko13 <144481877+TomoyaWaniko13@users.noreply.github.com>
2024-05-20 04:12:01 +0900

    Delete ticket-app-2 directory

--- ticket-app-2/components/AssignTicket.tsx
+++
@@ -1,7 +0,0 @@
-import { Ticket, User } from '@prisma/client';
-
-const AssignTicket = ({ ticket, users }: { ticket: Ticket; users: User[] }) => {
-  return <div>AssignTicket</div>;
-};
-
-export default AssignTicket;
