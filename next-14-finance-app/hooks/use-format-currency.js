c48d28c3e7545abda23e93187301fba93635dcdb
TomoyaWaniko <alligatorfree12@gmail.com>
2024-05-29 14:01:28 +0900

    update

--- next-14-finance-app/hooks/use-format-currency.js
+++
@@ -1,8 +0,0 @@
-import { useMemo } from 'react';
-
-export const useFormatCurrency = (amount) => {
-  const formatCurrency = (amount) =>
-    new Intl.NumberFormat('en-US', { style: 'currency', currency: 'CAD' }).format(amount);
-
-  return useMemo(() => formatCurrency(amount), [amount]);
-};
